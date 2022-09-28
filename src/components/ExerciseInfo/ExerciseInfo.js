bffd4be7a52e6249c819c9ad6771607067c33e6f
MD-Baki <bakimd77@gmail.com>
2022-09-29 01:42:11 +0600

    font awesone add & simple change

--- src/components/ExerciseInfo/ExerciseInfo.js
+++ src/components/ExerciseInfo/ExerciseInfo.js
@@ -1,3 +1,5 @@
+import { faCheck, faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ExerciseInfo.css'
 
@@ -8,6 +10,16 @@ const ExerciseInfo = (props) => {
     return (
         <div className='exercise-info'>
             <img src={img} alt="" />
+            <div className="exercise-details">
+                <h4>{name}</h4>
+                <p>{details}</p>
+                <h6>For Age: {age}</h6>
+                <h6>Time Required: {duration}s</h6>
+            </div>
+            <button>
+                <p>Add To List</p>
+                <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
